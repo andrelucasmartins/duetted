8df09ea39d325b53e7a9b970a3c7c9bd99fa4c4f
andrelucasmartins <andrelucasmartins@hotmail.com>
2024-11-21 21:38:04 -0300

    feat(layout.tsx): Add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,23 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Só Delicia",
-  description: "Aqui você encontra tudo sobre doces e salgados e receitas...",
+  description: "Aqui você encontra tudo sobre bolo, salgados, doces e receitas.",
+  keywords: ["doces", "salgados", "receitas", "bolos"],
+  authors: [{ name: 'André Ezequiel', url: 'https://github.com/andre-ezequiel' }],
+
+  viewport: 'width=device-width, initial-scale=1',
+
+  openGraph: {
+    title: 'Duetted recipes App',
+    description: 'Aqui você encontra tudo sobre doces e salgados e receitas...',
+    type: 'website',
+    locale: 'pt_BR',
+    siteName: 'Toast App'
+  },
+  robots: {
+    index: true,
+    follow: true
+  }
 };
 
 export default function RootLayout({
